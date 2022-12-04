4059dda5bf3f4edb84113bef02843916114b1f9b
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-04 11:08:42 +0100

    🎉 This is where it all begins... 🎉

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+        const [count, setCount] = useState(0);
+
+        return <main className='App'>container</main>;
+}
+
+export default App;
