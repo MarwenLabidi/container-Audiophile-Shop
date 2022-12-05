9fca9fd542e563864807fa81ee473353a05df618
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-05 19:23:40 +0100

    🚧 Work in progress : setting module federation plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,1 @@
-import { StyledContainer } from "./setup/styled-components/styled_component";
-import GlobalStyle from "./setup/styled-components/GlobalStyle";
-//import sub app header module federation
-// import {mount} from "headerSubApp/HeaderApp";//name/exposeName
-
-
-function Apps() {
-        return (
-                <>
-                        <GlobalStyle />
-                        <StyledContainer>containers
-                                {/* <App /> */}
-                        </StyledContainer>
-                </>
-        );
-}
-
-export default Apps;
+import("./container");
